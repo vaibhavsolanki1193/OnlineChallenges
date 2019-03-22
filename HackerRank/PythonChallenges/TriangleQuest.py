0a28f261cbb084e15d119d620f7ab4e8e896eab6
vaibhavsolanki1193 <sancuriousvaibhav@gmail.com>
2019-03-22 22:19:31 +0530

    Completed TraingleQuest

---
+++ HackerRank/PythonChallenges/TriangleQuest.py
@@ -0,0 +1,3 @@
+#https://www.hackerrank.com/challenges/python-quest-1/problem
+for i in range(1,10): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((10**(i)//9)*i)
