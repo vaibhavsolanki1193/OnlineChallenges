9b647a6368147b361268957ca77cfd8f1bc14678
Vaibhav Solanki <vaibhavsolanki1193@gmail.com>
2020-02-01 03:22:32 +0530

    re_split() formatting fixed

--- HackerRank/PythonChallenges/re_split.py
+++ HackerRank/PythonChallenges/re_split.py
@@ -9,5 +9,8 @@ print("\n".join(re.split(regex_pattern, input())))
 r"""
 import re
 print(*filter(None, re.split(r'[.,]+', input())), sep='\n')
-filter() returns every element in the second argument for which the function in the first argument evaluates as true. Using None as the first argument removes all items that are equivalent to false. The latter two test cases have consecutive delimiters, so using re.split() creates empty elements in the list. filter() returns the list without those empty elements.
+filter() returns every element in the second argument for which the function in the first argument evaluates as true.
+ Using None as the first argument removes all items that are equivalent to false. The latter two test cases have 
+ consecutive delimiters, so using re.split() creates empty elements in the list. filter() returns the list without those 
+ empty elements.
 """
