2c7b5f48f3ef9929b9013ef70a84641a1ab2a699
Vaibhav Solanki <vaibhavsolanki1193@gmail.com>
2020-02-23 05:50:37 +0530

    validating Postal code completed

---
+++ HackerRank/PythonChallenges/validatingPostalCode.py
@@ -0,0 +1,21 @@
+"""https://www.hackerrank.com/challenges/validating-postalcode/problem"""
+
+""" objective : Using regex, validate postal codes 
+ must be a number in the range from 100000 to 999999 inclusive.
+ must not contain more than one alternating repetitive digit pair.
+ 
+121426 # Here, 1 is an alternating repetitive digit.
+523563 # Here, NO digit is an alternating repetitive digit.
+552523 # Here, both 2 and 5 are alternating repetitive digits.
+
+"""
+
+regex_integer_in_range = r"^[1-9][0-9]{5}$"	# Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"	# Do not delete 'r'.
+
+
+import re
+P = input()
+
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
