fde0811d7c7dcbf05860ddd1df22487ac7d21bcb
Vaibhav Solanki <vaibhavsolanki1193@gmail.com>
2020-08-16 21:15:22 +0530

    Day 12 Done

--- HackerRank/30 Days of Coding/Day 12 Inheritance.py
+++ HackerRank/30 Days of Coding/Day 12 Inheritance.py
@@ -1,5 +1,4 @@
 # problem on https://www.hackerrank.com/challenges/30-inheritance/problem
-
 class Person:
 	def __init__(self, firstName, lastName, idNumber):
 		self.firstName = firstName
@@ -10,22 +9,24 @@ class Person:
 		print("ID:", self.idNumber)
 
 class Student(Person):
-    pass
-    #   Class Constructor
-    #   
-    #   Parameters:
-    #   firstName - A string denoting the Person's first name.
-    #   lastName - A string denoting the Person's last name.
-    #   id - An integer denoting the Person's ID number.
-    #   scores - An array of integers denoting the Person's test scores.
-    #
-    # Write your constructor here
-    
+    def __init__(self, firstName, lastName, idNumber, score):
+        self.score = score
+        Person.__init__(self, firstName, lastName, idNumber)
 
-    #   Function Name: calculate
-    #   Return: A character denoting the grade.
-    #
-    # Write your function here
+    def calculate(self):
+        result = sum(self.score) / len(self.score)
+        if result >= 90:
+            return 'O'
+        elif result < 90 and result >=80:
+            return 'E'
+        elif result < 80 and result >=70:
+            return 'A'
+        elif result < 70 and result >=55:
+            return 'P'
+        elif result < 55 and result >=40:
+            return 'D'
+        else:
+            return 'T'
 
 line = input().split()
 firstName = line[0]
