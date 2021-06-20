dbe07d51cb65b0de25367a704c8e4c7647c0c0fe
Vaibhav Solanki <vaibhavsolanki1193@gmail.com>
2021-06-20 13:17:15 +0530

    Algo expert added

---
+++ HackerRank/AlgoExpert/validateSubsequence.py
@@ -0,0 +1,11 @@
+def isValidSubsequence(array, sequence):
+    # Write your code here.
+    pass
+
+
+arr1 = int(input().replace('[','').replace(']','').split(','))
+
+print(arr1)
+print(type(arr1[1]))
+print(arr1[1])
+print("New")
